50e55804120bbbedf87d3692bc5f0de5dc9447a8
Layla Hashmi <laylahashmi@gmail.com>
2023-05-20 22:46:04 -0500

    updated css for Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Coder from '@/components/Coder'
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 xl:items-center">
       <Head>
         <title>Layla's Portfolio</title>
       </Head>
