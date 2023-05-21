f2d94444f4386b32eee516506fd5da8bb2885812
Layla Hashmi <laylahashmi@gmail.com>
2023-05-21 00:07:09 -0500

    updated background styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Coder from '@/components/Coder'
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(21,21,21)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Layla's Portfolio</title>
       </Head>
