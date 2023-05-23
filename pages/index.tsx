00382c6fee1f382460ba0b40799e289faa515c9d
Layla Hashmi <laylahashmi@gmail.com>
2023-05-22 19:14:11 -0500

    code to connect sanity to frotend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
-import Head from 'next/head'
-import Header from '@/components/Header'
-import Coder from '@/components/Coder'
-import About from '@/components/About'
-import WorkExperience from '@/components/WorkExperience'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import ContactMe from '@/components/ContactMe'
-import Link from 'next/link'
-import { HomeIcon } from '@heroicons/react/24/solid'
+import Head from 'next/head';
+import Header from '@/components/Header';
+import Coder from '@/components/Coder';
+import About from '@/components/About';
+import WorkExperience from '@/components/WorkExperience';
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
+import Link from 'next/link';
+import { HomeIcon } from '@heroicons/react/24/solid';
+import { GetStaticProps } from 'next';
+import { Experience, PageInfo, Skill, Project, Social } from '@/typings';
+import { fetchPageInfo } from '@/utils/fetchPageInfo';
+import { fetchExperiences } from '@/utils/fetchExperiences';
+import { fetchSkills } from '@/utils/fetchSkills';
+import { fetchProjects } from '@/utils/fetchProjects';
+import { fetchSocials } from '@/utils/fetchSocials';
 
-export default function Home() {
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
+
+const Home = (pageInfo, experiences, projects, skills, socials: Props) => {
   return (
     <div className="bg-[rgb(21,21,21)] text-white h-screen snap-y snap-mandatory overflow-y-scroll
     overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#22a4ef]/80
@@ -18,7 +33,7 @@ export default function Home() {
         <title>Layla's Portfolio</title>
       </Head>
 
-      <Header />
+      <Header socials={socials}/>
 
       <section id="coder" className="snap-start">
         <Coder />
@@ -54,4 +69,27 @@ export default function Home() {
     </div>
 
   )
-}
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    }
+  }
+
+  revalidate: 60;
+};
