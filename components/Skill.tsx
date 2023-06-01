fb3ac8d7d96300b42a97ff74d17e394f3c50e232
Layla Hashmi <laylahashmi@gmail.com>
2023-06-01 14:19:44 -0500

    added sanity data to skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import robinhoodLogo from '/public/robinhoodLogo.png'
 import Image from 'next/image'
+import { Skill } from '@/typings'
+import { urlFor } from '@/sanity'
 
 type Props = {
+    skill: Skill;
     directionLeft?: boolean;
 }
 
-function Skill({directionLeft}: Props) {
+function Skill({ skill, directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
         <motion.div
@@ -21,13 +24,16 @@ function Skill({directionLeft}: Props) {
             className="rounded-full border border-gray-500 object-cover w-24
             h-24 md:h-28 md:w-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300
             ease-in-out"
-            src={robinhoodLogo}
-            alt=""/>
+            src={urlFor(skill?.image).url()}
+            alt=""
+            height="100"
+            width="400"
+            />
         </motion.div>
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300
         ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
-                <p className="text-3xl font-bold text-black opacity-100">100%</p>
+                <p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
             </div>
         </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Skill from './Skill'
+import { Skill as SkillType } from '@/typings';
 
-type Props = {}
+type Props = {
+  skills: SkillType[];
+}
 
-function Skills({}: Props) {
+function Skills({ skills }: Props) {
   return (
-    <div
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1.5}}
     className="flex flex-col relative min-h-screen text-center md:text-lft xl:flex-row max-w-[2000px]
-    justify-evenly mx-auto items-center xl-space-y xl:px-10">
+    justify-evenly mx-auto items-center xl-space-y-0 xl:px-10">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
             Skills
         </h3>
@@ -18,24 +24,15 @@ function Skills({}: Props) {
         </h3>
 
         <div className="grid grid-cols-4 gap-5">
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
+          {skills?.slice(0, skills.length / 2).map((skill) => (
+            <Skill key={skill._id} skill={skill} />
+          ))}
 
+          {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+            <Skill key={skill._id} skill={skill} directionLeft />
+          ))}
         </div>
-    </div>
+    </motion.div>
   )
 }
 
