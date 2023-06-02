f56d912ae394bd290bda035a3c70d07ec275fcb7
Layla Hashmi <laylahashmi@gmail.com>
2023-06-02 15:16:59 -0500

    set up nodemailer for contact form

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,20 +1,49 @@
 import React from 'react'
+import { useState } from 'react'
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 import { useForm, SubmitHandler } from "react-hook-form";
+import { sendContactForm } from '../lib/api'
 
 
-type Inputs = {
+interface Props {
     name: string;
     email: string;
-    subject: string;
     message: string;
   };
 
-type Props = {}
+export default function ContactMe() {
+    const [isLoading, setisLoading] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+    });
 
-export default function ContactMe({}: Props) {
-    const { register, handleSubmit } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = formData => console.log(formData);
+    const onSubmit = async (data: Props) => {
+        console.log(data);
+        try {
+            setisLoading(true);
+            const res = await fetch('/api/contact', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const body = await res.json();
+
+            if (res.ok) {
+                alert(`${body.message} 🚀`)
+            }
+        }
+    };
 
   return (
     <div className="h-screen relative text-center flex flex-col md:text-left md:flex-row px-10 max-w-7xl
@@ -25,7 +54,7 @@ export default function ContactMe({}: Props) {
 
         <div className="flex flex-col space-y-10">
             <h4 className="text-4xl font-semibold text-center">
-                I've got just what you need.{" "}
+                Liking what you see so far?{" "}
                 <span className="underline decoration-[#22a4ef]/50">{" "}Let's talk.</span>
             </h4>
 
@@ -48,13 +77,11 @@ export default function ContactMe({}: Props) {
 
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
                 <div className="flex space-x-2">
-                    <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
-                    <input {...register('email')} placeholder="Email" className="contactInput" type="email" />
+                    <input required id="name" {...register('name')} placeholder="Name" className="contactInput" type="text" />
+                    <input required id="email" {...register('email')} placeholder="Email" className="contactInput" type="email" />
                 </div>
 
-                <input {...register('subject')} placeholder="Subject" className="contactInput" type="text"/>
-
-                <textarea {...register('message')} placeholder="Message" className="contactInput" />
+                <textarea required id="message" {...register('message')} placeholder="Message" className="contactInput" />
 
                 <button type="submit" className="bg-[#22a4ef] py-5 px-10 rounded-md text-black font-bold text-lg">Submit</button>
             </form>
---
+++ pages/api/contact.ts
@@ -0,0 +1,43 @@
+// import { mailOptions, transporter } from "../../config/nodemailer";
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+
+const myemail = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: myemail,
+        pass,
+    },
+});
+
+  const { name, email, message } = req.body;
+  const mailData = {
+    from: myemail,
+    to: myemail,
+    subject: `Message from ${name}`,
+    text: `${message} | Sent from: ${email}`,
+    html: `<div>${message}</div><p>Sent from: ${email}</p>`,
+};
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err: Error | null, info) => {
+      if (err) {
+        reject(err);
+        return res.status(500).json({ error:message || 'Something went wrong' });
+      } else {
+        resolve(info.accepted);
+        res.status(200).json({message: 'Message sent!'});
+    }
+  });
+});
+
+  return;
+}
